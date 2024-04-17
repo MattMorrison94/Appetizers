e45fa30616cc6dedf803367222f0454d97e4fff5
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-04-17 15:11:52 +0930

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Matthew Morrison on 17/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
